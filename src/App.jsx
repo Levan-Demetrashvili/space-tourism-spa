98a5d43ff6d3ea0573ef6a8f4eaef612be7a4214
Levan-Demetrashvili <levandemetrashvili10@gmail.com>
2024-08-06 17:44:47 +0400

    Initialize project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
