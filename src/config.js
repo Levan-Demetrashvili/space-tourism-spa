f391c0ecb0c3bc470ae83864b2b259360dede120
Levan-Demetrashvili <levandemetrashvili10@gmail.com>
2024-08-23 15:57:10 +0400

    Fix the API URL

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const BASE_API_URL = 'my-json-server.typicode.com/Levan-Demetrashvili/space-tourism-spa/';
+export const BASE_API_URL = 'https://my-json-server.typicode.com/Levan-Demetrashvili/space-tourism-spa/';
